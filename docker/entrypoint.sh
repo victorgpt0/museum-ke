acb8cce5ef623dfd8d7dd0d748c0da59b908cde8
victorgpt0 <175977320+victorgpt0@users.noreply.github.com>
2025-12-26 22:56:49 +0300

    as opposed to kms

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,12 +3,18 @@ set -e
 
 echo "Starting Postgresql..."
 
-su - postgres -c "pg_ctl -D /var/lib/postgresql/data -l /var/lib/postgresql/logfile start"
+PG_VERSION=$(ls /usr/lib/postgresql/ | head -n 1)
+PG_BIN="/usr/lib/postgresql/$PG_VERSION/bin"
 
-su - postgres -c "psql -c \"CREATE DATABASE ${DB_DATABASE};\"" || echo "Database already exists"
-su - postgres -c "psql -c \"CREATE USER ${DB_USERNAME} WITH PASSWORD '${DB_PASSWORD}';\"" 2>/dev/null || echo "User already exists"
-su - postgres -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE ${DB_DATABASE} TO ${DB_USERNAME};\"" 2>/dev/null || true
-su - postgres -c "psql -d ${DB_DATABASE} -c \"GRANT ALL ON SCHEMA public TO ${DB_USERNAME};\"" 2>/dev/null || true
+runuser -u postgres -- $PG_BIN/pg_ctl -D /var/lib/postgresql/data -l /var/lib/postgresql/logfile start
+
+sleep 3
+
+runuser -u postgres -- psql -c "CREATE DATABASE ${DB_DATABASE};" 2>/dev/null || echo "Database already exists"
+runuser -u postgres -- psql -c  "CREATE USER ${DB_USERNAME} WITH PASSWORD '${DB_PASSWORD}';" 2>/dev/null || echo "User already exists"
+runuser -u postgres -- psql -c  "GRANT ALL PRIVILEGES ON DATABASE ${DB_DATABASE} TO ${DB_USERNAME};\"" 2>/dev/null || true
+runuser -u postgres -- psql -d ${DB_DATABASE} -c "GRANT ALL ON SCHEMA public TO ${DB_USERNAME};" 2>/dev/null || true
+runuser -u postgres -- psql -d ${DB_DATABASE} -c "ALTER SCHEMA public OWNER TO ${DB_USERNAME};" 2>/dev/null || true
 
 echo "Starting Laravel application..."
 
