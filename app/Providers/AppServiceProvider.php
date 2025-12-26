f9859c42b6657bede2c829ead2e738935fc6a35e
victorgpt0 <175977320+victorgpt0@users.noreply.github.com>
2025-12-26 21:18:48 +0300

    remove telescope

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,11 +20,6 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton('hashids', function () {
             return new Hashids(config('hashids.salt'), config('hashids.min_length'));
         });
-
-        if ($this->app->isLocal()) {
-            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
-            $this->app->register(TelescopeServiceProvider::class);
-        }
     }
 
     /**
