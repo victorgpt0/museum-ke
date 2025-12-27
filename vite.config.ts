1638b07b472a75c62143bd16b991d9514342ac3a
victorgpt0 <175977320+victorgpt0@users.noreply.github.com>
2025-12-27 21:22:12 +0300

    npm run build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
