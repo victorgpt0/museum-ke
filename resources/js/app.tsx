43e36f06a25fb804d308ba11b3e90c13aeeaade9
victorgpt0 <175977320+victorgpt0@users.noreply.github.com>
2025-12-28 20:38:28 +0300

    echo reverb config

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -6,10 +6,16 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
 
-// configureEcho({
-//     broadcaster: 'reverb',
-//     key: import.meta.env.VITE_REVERB_APP_KEY,
-// });
+configureEcho({
+    broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT ? parseInt(import.meta.env.VITE_REVERB_PORT) : undefined,
+    wssPort: import.meta.env.VITE_REVERB_PORT ? parseInt(import.meta.env.VITE_REVERB_PORT) : undefined,
+    forceTLS: import.meta.env.VITE_REVERB_SCHEME === 'https',
+    disableStats: true,
+    enabledTransports: ['ws', 'wss'],
+});
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
